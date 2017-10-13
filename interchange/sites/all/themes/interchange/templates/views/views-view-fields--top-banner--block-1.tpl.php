2fbaef3f9eb4358f5e1bccb85da8515189879fa5
Sonic <lovesonic@126.com>
2017-10-13 22:51:56 +0800

    too lazy to type

---
+++ interchange/sites/all/themes/interchange/templates/views/views-view-fields--top-banner--block-1.tpl.php
@@ -0,0 +1,10 @@
+<div class="tpobanner-box">
+  <div class="topbanner-thumb">
+	<?php print $fields['field_thumb']->content ?>
+  </div>
+  <div class="topbanner-info">
+    <div class="article-title">
+		<?php print $fields['title']->content ?>
+    </div>
+  </div>
+</div>

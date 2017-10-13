000e60f0732442512f3027545d0e3cb0842c78bf
Sonic <lovesonic@126.com>
2017-10-13 14:53:49 +0800

    too lazy to type

--- interchange/sites/all/themes/interchange/templates/page--front.tpl.php
+++ interchange/sites/all/themes/interchange/templates/page--front.tpl.php
@@ -106,9 +106,9 @@
 			  <?php if (!empty($secondary_nav)): ?>
 				<?php print render($secondary_nav); ?>
 			  <?php endif; ?>
-<!-- 			  <?php if (!empty($page['navigation'])): ?>
+			  <?php if (!empty($page['navigation'])): ?>
 				<?php print render ($page['navigation']); ?>
-			  <?php endif; ?> -->
+			  <?php endif; ?>
 			</nav>
 		  </div>
 		<?php endif; ?>	

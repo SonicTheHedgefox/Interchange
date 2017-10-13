c60c197704c40f9d34fac380a7a71be55f793394
Sonic <lovesonic@126.com>
2017-10-13 16:54:28 +0800

    too lazy to type

--- interchange/sites/all/themes/interchange/templates/views/views-view-fields--recommend-box--block.tpl.php
+++ interchange/sites/all/themes/interchange/templates/views/views-view-fields--recommend-box--block.tpl.php
@@ -1,15 +1,10 @@
 <div class="recommend-box">
-  <div class="course-img">
+  <div class="recommend-thumb">
 	<?php print $fields['field_thumb']->content ?>
   </div>
-  <div class="course-info">
-    <div class="course-title">
+  <div class="article-info">
+    <div class="article-title">
 		<?php print $fields['title']->content ?>
     </div>
-    <div class="metas clearfix">
-		<span class="post-user"><?php print $fields['name']->content ?></span>
-        <span class="icon_num"><i class="fa fa-eye" aria-hidden="true"></i><i class="icon_after"> </i></span>
-        <span class="icon_num"><i class="fa fa-comments-o" aria-hidden="true"></i><i class="icon_after"> </i></span>	    
-	</div>
   </div>
 </div>

57896d128bbe3266f5e8aef5a5a2f9253b65fe75
Sonic <lovesonic@126.com>
2017-10-14 14:03:44 +0800

    too lazy to type

--- interchange/sites/all/themes/bootstrap/templates/system/page.tpl.php
+++ interchange/sites/all/themes/bootstrap/templates/system/page.tpl.php
@@ -148,9 +148,7 @@
       <?php if (!empty($page['help'])): ?>
         <?php print render($page['help']); ?>
       <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
+ 
       <?php print render($page['content']); ?>
     </section>
 
--- interchange/sites/all/themes/interchange/templates/page.tpl.php
+++ interchange/sites/all/themes/interchange/templates/page.tpl.php
@@ -118,10 +118,9 @@
 	 	<div class="main-content">
 				<div class="content-list col-md-9">
 		  			
-
-		  			<?php if (!empty($page['highlighted'])): ?>
-       				<div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      				<?php endif; ?>
+			     	<?php if (!empty($action_links)): ?>
+			        <ul class="action-links"><?php print render($action_links); ?></ul>
+			     	<?php endif; ?>
 		  			<?php print render($page['content']); ?>
 				
 				</div>

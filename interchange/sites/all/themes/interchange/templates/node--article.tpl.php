99fcf8d378aa87a9ace06ba6bf4b5439e960ef20
Sonic <lovesonic@126.com>
2017-10-15 00:57:26 +0800

    too lazy to type

--- interchange/sites/all/themes/interchange/templates/node--article.tpl.php
+++ interchange/sites/all/themes/interchange/templates/node--article.tpl.php
@@ -84,7 +84,7 @@
   <header>
    <?php if (!empty($title)): ?><h2 class="page-header"><?php print $title; ?></h2><?php endif; ?>
    <div class="node-info">
-      <span class="node-info"><?php print format_date($created ,'custom','Y , n, j'); ?></span>
+      <span class="node-info"><?php print format_date($created ,'custom','Y-n-j'); ?></span>
 
       <span class="node-info"> <?php print $user_picture; ?> </span>
       <span class="node-info"><?php print render($name);?></span>      

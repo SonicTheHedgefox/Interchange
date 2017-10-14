ac51c3aaae48cab4fe33cf86651e235ffa0bdb7e
Sonic <lovesonic@126.com>
2017-10-14 14:05:04 +0800

    too lazy to type

--- interchange/sites/all/themes/bootstrap/templates/system/page.tpl.php
+++ interchange/sites/all/themes/bootstrap/templates/system/page.tpl.php
@@ -133,7 +133,9 @@
     <?php endif; ?>
 
     <section<?php print $content_column_class; ?>>
-   
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
@@ -142,11 +144,15 @@
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
- 
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
       <?php if (!empty($page['help'])): ?>
         <?php print render($page['help']); ?>
       <?php endif; ?>
- 
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
       <?php print render($page['content']); ?>
     </section>
 
--- interchange/sites/all/themes/interchange/templates/page.tpl.php
+++ interchange/sites/all/themes/interchange/templates/page.tpl.php
@@ -118,9 +118,9 @@
 	 	<div class="main-content">
 				<div class="content-list col-md-9">
 		  			
-			     	<?php if (!empty($action_links)): ?>
-			        <ul class="action-links"><?php print render($action_links); ?></ul>
-			     	<?php endif; ?>
+					<?php if (!empty($tabs)): ?>
+					<?php print render($tabs); ?>
+					<?php endif; ?>
 		  			<?php print render($page['content']); ?>
 				
 				</div>

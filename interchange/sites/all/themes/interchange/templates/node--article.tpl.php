9644ce392ffd5d22866b453684f1caf440a9b400
Sonic <lovesonic@126.com>
2017-10-15 00:46:41 +0800

    too lazy to type

--- interchange/sites/all/themes/interchange/templates/node--article.tpl.php
+++ interchange/sites/all/themes/interchange/templates/node--article.tpl.php
@@ -84,7 +84,8 @@
   <header>
    <?php if (!empty($title)): ?><h2 class="page-header"><?php print $title; ?></h2><?php endif; ?>
    <div class="node-info">
-      <span class="node-info"><?php print format_date($date ,'custom','F j, Y'); ?></span>
+      <span class="node-info"><?php print render($date);?></span>
+      
       <span class="node-info"><?php print $user_picture; ?> </span>
       <span class="node-info"><?php print render($name);?></span>      
    </div>

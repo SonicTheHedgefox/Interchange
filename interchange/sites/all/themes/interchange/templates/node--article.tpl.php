308446702fed216498f736587b7c5c6e7c2ef231
Sonic <lovesonic@126.com>
2017-10-15 00:48:53 +0800

    too lazy to type

--- interchange/sites/all/themes/interchange/templates/node--article.tpl.php
+++ interchange/sites/all/themes/interchange/templates/node--article.tpl.php
@@ -86,7 +86,7 @@
    <div class="node-info">
       <span class="node-info"><?php echo date_format($date,"Y/m/d");?></span>
 
-      <span class="node-info"><?php print $user_picture; ?> </span>
+      <span class="node-info"> <?php print $user_picture; ?> </span>
       <span class="node-info"><?php print render($name);?></span>      
    </div>
 

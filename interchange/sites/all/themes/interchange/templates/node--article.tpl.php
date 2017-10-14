83e45b3fd0bc1db4d95b8c3add18f25beb236cb0
Sonic <lovesonic@126.com>
2017-10-15 00:52:36 +0800

    too lazy to type

--- interchange/sites/all/themes/interchange/templates/node--article.tpl.php
+++ interchange/sites/all/themes/interchange/templates/node--article.tpl.php
@@ -95,12 +95,7 @@
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
-    <?php if ($display_submitted): ?>
-    <span class="submitted">
-      <?php print $user_picture; ?>
-      <?php print $submitted; ?>
-    </span>
-    <?php endif; ?>
+
   </header>
   <?php endif; ?>
   <?php

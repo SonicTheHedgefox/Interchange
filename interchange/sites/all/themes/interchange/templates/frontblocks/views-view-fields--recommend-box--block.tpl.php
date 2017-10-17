4c4cc154da1ec9ab80f7e3ce2dfc833825d21279
Sonic <lovesonic@126.com>
2017-10-18 01:43:11 +0800

    too lazy to type

--- interchange/sites/all/themes/interchange/templates/frontblocks/views-view-fields--recommend-box--block.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="article-box">
-  <div class="article-thumb">
-	<?php print $fields['field_thumb']->content ?>
-  </div>
-  <div class="article-info">
-		<?php print $fields['title']->content ?>
-  </div>
-</div>

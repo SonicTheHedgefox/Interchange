01221916f8001e994e4ca4475a37f70f38a591dc
Sonic <lovesonic@126.com>
2017-10-23 23:05:27 +0800

    too lazy to type

--- interchange/sites/all/themes/interchange/templates/page.tpl.php
+++ interchange/sites/all/themes/interchange/templates/page.tpl.php
@@ -161,7 +161,7 @@
 
 	    <?php if (!empty($page['sidebar_second'])): ?>
 	      <aside class="col-sm-3" role="complementary">
-	        <?php print render($page['sidebar_second']); ?>
+	        <?php print render($page['sidebar_first']); ?>
 	      </aside>  <!-- /#sidebar-second -->
 	    <?php endif; ?>
 	  </div>

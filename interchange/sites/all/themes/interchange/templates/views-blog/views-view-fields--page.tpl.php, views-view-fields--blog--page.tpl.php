1a543725e2a0830d2fe47caed59da9bec0249c3f
Sonic <lovesonic@126.com>
2017-10-26 21:10:34 +0800

    too lazy to type

---
+++ interchange/sites/all/themes/interchange/templates/views-blog/views-view-fields--page.tpl.php, views-view-fields--blog--page.tpl.php
@@ -0,0 +1,18 @@
+<div class="blog-box box-c">
+
+  <div class="blog-thumb">
+  <?php print $fields['field_thumb']->content ?>
+  </div>
+
+  <div class="blog-info">
+  <h4 class="blog-box-title"><?php print $fields['title']->content ?> </h4>
+    <span class="blog-box-author">
+    <?php print $fields['name']->content ?> 
+    </span>
+    
+    <span class="blog-box-time">
+      <?php print $fields['changed']->content ?>
+    </span>
+  </div>
+<div style="clear: both;"></div>
+</div>

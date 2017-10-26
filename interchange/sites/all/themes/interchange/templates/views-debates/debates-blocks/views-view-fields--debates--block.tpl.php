56b4da7e19a84ad022d9b5dfd00ceca017d3eb40
Sonic <lovesonic@126.com>
2017-10-26 15:58:35 +0800

    too lazy to type

---
+++ interchange/sites/all/themes/interchange/templates/views-debates/debates-blocks/views-view-fields--debates--block.tpl.php
@@ -0,0 +1,17 @@
+<div class="article-box box-b col-md-6">
+
+  <div class="article-thumb">
+	<?php print $fields['field_thumb']->content ?>
+  </div>
+
+  <div class="article-info">
+   <span class="article-tag">
+      <i class="fa fa-tag" aria-hidden="true"></i>
+      <?php print $fields['field_tags']->content ?>
+  </span>
+		<?php print $fields['title']->content ?>
+    <?php print $fields['name']->content ?>
+    <?php print $fields['changed']->content ?>
+  </div>
+
+</div>

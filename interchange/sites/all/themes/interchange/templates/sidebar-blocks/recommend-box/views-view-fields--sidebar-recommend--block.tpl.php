0b07a7b70c5c815980a76ce6d722a84923bb52e9
Sonic <lovesonic@126.com>
2017-10-26 19:09:24 +0800

    too lazy to type

---
+++ interchange/sites/all/themes/interchange/templates/sidebar-blocks/recommend-box/views-view-fields--sidebar-recommend--block.tpl.php
@@ -0,0 +1,8 @@
+<div class="sidebar-box box-d col-md-6">
+  <div class="sidebar-thumb">
+  <?php print $fields['field_thumb']->content ?>
+  </div>
+  <div class="sidebar-info">
+    <h4 class="article-box-title"><?php print $fields['title']->content ?> </h4>
+  </div>
+</div>
